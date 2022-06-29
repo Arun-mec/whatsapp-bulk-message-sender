37007c897c08750e47a7197222a5df56e69e7801
Arun-mec <arunalakkalpalla@gmail.com>
2022-06-29 14:51:28 +0530

    final working ou

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { Client, MessageMedia, LocalAuth  } = require('whatsapp-web.js')
 const QRCode = require('qrcode');
+const csvToJson = require('convert-csv-to-json');
 const fileUpload = require('express-fileupload');
 router.use(fileUpload())
 
@@ -29,28 +30,32 @@ router.get('/', function(req, res, next) {
 });
 router.post('/getqrcode', function(req, res, next) {
   let medMsg = req.files.img;
-  let nums = req.files.nums;
+  let num_csv = req.files.nums;
   let msg = req.body.msg;
   // moving the images to public folder to access it later
   medMsg.mv('./public/images/'+'medMsg.jpg', function(err) {
     if (err)
       return res.send(err);
   });
+  num_csv.mv('./public/csvs/'+'nums.csv',function(err) {
+    if (err)
+      return res.send(err);
+  });
 
-  console.log(msg);
+  const med_msg = MessageMedia.fromFilePath('./public/images/'+'medMsg.jpg')
+  const nums = csvToJson.fieldDelimiter(',',' ').getJsonFromCsv('./public/csvs/'+'nums.csv');
+  
   client.on('qr', qr => {
     QRCode.toDataURL(qr, (err, url)=>{
       res.render("success",{image_url : url})
     } )
   });
 
-  num_set = ['9961692453','7356469164']
   client.on('ready', () => {
     console.log('Client is ready!');
-    
-    const med_msg = MessageMedia.fromFilePath('./public/images/'+'medMsg.jpg')
-    num_set.forEach((obj)=>{
-      const num_id = '91'+obj+"@c.us"
+
+    nums.forEach((obj)=>{
+      const num_id = obj.Number.substring(1)+"@c.us";
       if (num_id.length > 10){
           client.sendMessage(num_id,msg)
           client.sendMessage(num_id,med_msg)
@@ -58,7 +63,7 @@ router.post('/getqrcode', function(req, res, next) {
       }else{
           console.log(obj.Number + " This is not a registered number");
         }
-  })
+    })
   });
   client.initialize();
   
