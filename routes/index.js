aa98c6929435ee42dc64b475634bb9c1a32509ec
Arun-mec <arunalakkalpalla@gmail.com>
2022-06-29 11:58:31 +0530

    puppeteer removed

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,41 @@
 var express = require('express');
 var router = express.Router();
 const { Client, MessageMedia, LocalAuth  } = require('whatsapp-web.js')
-const client = new Client({
-  restartOnAuthFail: true,
-  puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process', // <- this one doesn't works in Windows
-      '--disable-gpu'
-    ],
-  },
-  authStrategy: new LocalAuth()
-});
 const QRCode = require('qrcode');
-img=""
+
+const client = new Client()
+//   {
+//   // restartOnAuthFail: true,
+//   puppeteer: {
+//     headless: true,
+//     args: [
+//       '--no-sandbox',
+//       '--disable-setuid-sandbox',
+//       '--disable-dev-shm-usage',
+//       '--disable-accelerated-2d-canvas',
+//       '--no-first-run',
+//       '--no-zygote',
+//       '--disable-gpu'
+//     ],
+//   },
+//   authStrategy: new LocalAuth()
+// });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/sendmessage', async function(req, res, next) {
+router.get('/sendmessage', function(req, res, next) {
+  
+  client.on('ready', () => {
+    console.log('Client is ready!');
+    client.sendMessage("919961692453@c.us","Hello")
+  });
   client.initialize();
   client.on('qr', qr => {
     QRCode.toDataURL(qr, (err, url)=>{
       res.render("success",{image_url : url})
     } )
   });
-  client.on('ready', () => {
-    console.log('Client is ready!');
-    client.sendMessage("919961692453@c.us","Hello")
-  });
-  
 });
 module.exports = router;
