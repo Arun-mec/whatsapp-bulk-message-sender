aed84fff150370f8700b412970c1e02fa13b901d
Arun-mec <arunalakkalpalla@gmail.com>
2022-07-05 12:31:48 +0530

    media messages are sent first and then text

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const csvToJson = require('convert-csv-to-json');
 const fileUpload = require('express-fileupload');
 router.use(fileUpload())
 
+// Opening the browser window using puppeteer
 const client = new Client({
-  // restartOnAuthFail: true,
   puppeteer: {
     headless: true,
     args: [
@@ -20,35 +20,39 @@ const client = new Client({
       '--disable-gpu'
     ],
   }
-  // authStrategy: new LocalAuth()
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// form submission process, a qr code will be generated and that has to ber scanned to login to the whatsapp
 router.post('/getqrcode', function(req, res, next) {
   let medMsg = req.files.img;
   let num_csv = req.files.nums;
   let msg = req.body.msg;
   console.log(msg)
+
   // moving the images to public folder to access it later
   medMsg.mv('./public/images/'+'medMsg.jpg', function(err) {
     if (err)
       return res.send(err);
   });
+  // moving the csv file to the pblic folder to acces it later
   num_csv.mv('./public/csvs/'+'nums.csv', function(err) {
     if (err)
       return res.send(err);
   });
 
-  
+  // QRCode is converted to image format.
   client.on('qr', qr => {
     QRCode.toDataURL(qr, (err, url)=>{
       res.render("success",{image_url : url})
     } )
   });
   
+  // messages will be sent from here.
   client.on('ready', () => {
     console.log('Client is ready!');
     const med_msg = MessageMedia.fromFilePath('./public/images/'+'medMsg.jpg')
@@ -56,14 +60,17 @@ router.post('/getqrcode', function(req, res, next) {
     nums.forEach((obj)=>{
       const num_id = obj.Number.substring(1)+"@c.us";
       if (num_id.length > 10){
-          client.sendMessage(num_id,msg)
-          client.sendMessage(num_id,med_msg)
+          client.sendMessage(num_id,med_msg) 
+          setTimeout(()=>{client.sendMessage(num_id,msg)},5000)// media message will be sent first 
+           // text messages will be sent later
           // console.log("Done")
       }else{
           console.log(obj.Number + " This is not a registered number");
         }
     })
   });
+
+  // initialising the client object
   client.initialize();
   
 });
